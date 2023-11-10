302449c3645f464007044d301a155759ede47c13
David Serrano <75829857+serranoio@users.noreply.github.com>
2023-11-09 20:16:33 -0600

    started list view

--- src/components/Pages/Map-component.ts
+++ src/components/Pages/Map-component.ts
@@ -1,4 +1,4 @@
-import {html, css, LitElement} from 'lit';
+import {html, css, LitElement, TemplateResult} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
 // A plain Lit component which can be used in any framework
@@ -7,14 +7,24 @@ export class MapComponent extends LitElement {
   declare name: string;
   declare center: any;
   declare map: any;
+  declare locations: any;
+  declare openListView: any;
+  declare isOpened: boolean;
 
   static styles = css`
+  :host {
+    position: relative;
+    z-index: 0;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
     p {
-      display: inline-block;
-      border: solid 1px gray;
-      background: white;
-      color: black;
-      padding: 0 1em;
+      display: block;
+      // background-color: black;
+      color: var(--c-text-color);
     }
 
 
@@ -22,7 +32,20 @@ export class MapComponent extends LitElement {
       width: 100vw;
       height: 100vh;
       z-index: 1;
+      overflow: hidden;
+      position: relative;
   }
+
+  .list-view {
+    position: absolute;
+    right: 0;
+    top: 50%;
+    font-size: var(--text-m);
+    background-color: green;
+    z-index: 9999;
+    cursor: pointer;
+  }
+
 
   .place {
       text-align: left;
@@ -35,10 +58,11 @@ export class MapComponent extends LitElement {
       background-color: rgba(0,0,0,.5);
       color: var(--c-text-color);
       position: relative;
-      font-size: 16px;
+      font-size: var(--text-xs);
       border-radius: 10px;
       backdrop-filter: blur(10px);
       transform: translateY(-10px);
+      
   }
   .here::after {
       content: "";
@@ -69,8 +93,15 @@ clip-path: polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%,
   }
 
 
+
+  .name-element {
+    margin-bottom: var(--space-s);
+    font-size: var(--text-s) !important;
+  }
+
     .link {
       color: var(--c-text-color);
+      text-decoration: none;
   }
 
   .gold span {
@@ -102,61 +133,108 @@ clip-path: polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%,
       z-index: 9999997;
   }
 
+  .drawer {
+    position: fixed;
+    height: 100%;
+    width: 100vw;
+    background-color: rgba(0,0,0,.5);
+    z-index: 9999999;
+    transform: translate(100%);
+    transition: all .2s;
+  }
+
+  .drawer.opened {
+
+    transform: translate(0%);
+  }
+
+
   `;
 
   static properties = {name: {type: String}}
 
 
-  firstUpdated(): void {
-    // super.firstUpdated()
-
-    // @ts-ignore
-    let mapElement: HTMLElement = this.renderRoot.getElementById("map")
-
-      // Request needed libraries.
-   this.map = new google.maps.Map(mapElement, {
-    center: this.center,
-    zoom: 15,
-    mapId: "4504f8b37365c3d0",
-  });
-
-  
-  // infowindow = new google.maps.InfoWindow();
-  
+// taken from code found online lol
+getDistanceFromLatLonInKm(lat1: number,lon1: number,lat2: number,lon2: number) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = this.deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
 }
 
+deg2rad(deg: any) {
+  return deg * (Math.PI/180)
+}
 
-async addMarkerElement(div: any, location: any) {
-  try {
-    // @ts-ignore
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+distance(rest: any) {
+ const d =  this.getDistanceFromLatLonInKm(rest.lat(), rest.lng(), this.center.lat, this.center.lng);
 
+ return d;
+}
 
-    console.log(this.map, div, location)
-
-    const marker = new AdvancedMarkerElement({
-      map: this.map,
-      position: location,
-      content: div,
-    });
-
-    console.log(marker)
-  } catch(e) {
-
+  byDistance(a: any, b: any) {
+    return a.distance - b.distance
   }
-}
+  
+  calcDistances(results: any) {
+  let newResults = [];
 
-setMarker(name: any, location: any, place: any, rank: any) {
+    for (let i = 0; i < results.length; i++) {
+      const newResult = {
+        ...results[i],
+        distance: this.distance(results[i].geometry.location)
+      }
+  
+      newResults.push(newResult);
+    }
 
-  const RestaurantInfoElement = document.createElement("div");
-  RestaurantInfoElement.classList.add("here");
-  RestaurantInfoElement.textContent = name;
+    return newResults.sort(this.byDistance.bind(this));
+  }
+
+  async addMarkerElement(div: any, location: any) {
+    try {
+      // @ts-ignore
+      const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+  
+      const marker = new AdvancedMarkerElement({
+        map: this.map,
+        position: location,
+        content: div,
+      });
+    } catch(e) {
+      
+    }
+  }
+  
+  async setMarker(name: any, location: any, place: any, rank: any) {
+    const RestaurantInfoElement = document.createElement("div");
+    RestaurantInfoElement.classList.add("here");
+    const link = document.createElement("a");
+  
+    link.target = "_blank";
+    link.href = "https://google.com";
+    link.classList.add("link")
+  
+  
+    RestaurantInfoElement.appendChild(link);
+
+    const nameElement = document.createElement("p");
+  nameElement.classList.add("name-element")
+  nameElement.textContent = name;
+  link.appendChild(nameElement);
   
   if (place) {
     RestaurantInfoElement.classList.add("place");
     const p = document.createElement("p");
-    // p.innerText = distance(location).toFixed(3) + " km"
-    RestaurantInfoElement.appendChild(p);
+    p.textContent = place.distance.toFixed(2) + " km ";
+    link.appendChild(p);
 
     rank = rank+1;
     const span = document.createElement("span");
@@ -170,66 +248,148 @@ setMarker(name: any, location: any, place: any, rank: any) {
       RestaurantInfoElement.classList.add("bronze");
     }
 
-    const link = document.createElement("a");
 
-    link.innerText = "Directions!"
-    link.target = "_blank";
-    link.href = "https://google.com";
-    link.classList.add("link")
-
-    console.log(place)
-
-    RestaurantInfoElement.appendChild(link);
-
-    RestaurantInfoElement.appendChild(span);
+    link.appendChild(span);
   }
      
-
-  this.addMarkerElement(RestaurantInfoElement, this.center);
+  this.addMarkerElement(RestaurantInfoElement, location);
+}
   
+  async checkIfLocExists(place: any, rank: any) {
+   if (!place.geometry || !place.geometry.location) return;
+   let marker;
+ 
+ try {
+   marker = await this.setMarker(place.name, place.geometry.location, place, rank)
+ } catch (e) {
+ 
+ } finally {
+ 
+   google.maps.event.addListener(marker!, "click", () => {
+     // infowindow.setContent(place.name);
+     // infowindow.open(map);
+   });  
+ }
+ }
 
+  makeRequest(newRequest: any) {
+    let request = {
+      keyword: "pizza",
+      type: "restaurant",
+      openNow: true,
+      fields: ["ALL"],
+      location: this.center,
+      radius: 1000,
+    };
+  
+    if (newRequest) {
+      request = newRequest
+    }
+  
+    let service = new google.maps.places.PlacesService(this.map);
+    service.nearbySearch(request, (results, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+        this.locations = this.calcDistances(results)
+        for (let i = 0; i < this.locations.length; i++) {
+          this.checkIfLocExists(this.locations[i], i);
+        }
+      }
+    });
+  } 
+
+
+  firstUpdated(): void {
+    // @ts-ignore
+    let mapElement: HTMLElement = this.renderRoot.getElementById("map")
+
+    // Request needed libraries.
+    navigator.geolocation.getCurrentPosition((e) => {
+      
+      const crd = e.coords;
+      this.center.lng = crd.longitude
+      this.center.lat = crd.latitude;
+      
+      console.log("got current position")
+      
+      this.map.setCenter(this.center);
+      this.setMarker("You are here", this.center, null, null);
+    
+    this.requestUpdate()
+    
+    this.makeRequest(null);
+    
+    }, () => {
+    
+    })
+
+   this.map = new google.maps.Map(mapElement, {
+    center: this.center,
+    zoom: 15,
+    mapId: "4504f8b37365c3d0",
+  });
 }
 
 constructor() {
   super();
   this.name = "";
   this.center = {lat: 0, lng: 0}
-  
-  
-  navigator.geolocation.getCurrentPosition((e) => {
+  }
+
+  openDrawer() {
+    this.isOpened = !this.isOpened;
+    this.requestUpdate()
+  }
+
+  getAside(): TemplateResult {
     
-    const crd = e.coords;
-    this.center.lng = crd.longitude
-    this.center.lat = crd.latitude;
+    console.log(this.locations)
+    if (!this.locations) {
+      return html``;
+    }
+
+
+    return html`<aside class="drawer ${this.isOpened ? "opened" : ""}">
     
-    console.log("got current position")
-    
-    this.map.setCenter(this.center);
-    this.setMarker("You are here", this.center, null, null);
-  
-  this.requestUpdate()
-  
-  // map.setCenter(myLatLng)
-  // setMarker("You are here", myLatLng)
-  
-  // makeRequest()
-  
-  
-}, () => {
-  
-})
+    <div class="padding">
+    <ol>
+    ${this.locations.map((location: any) => {
+      
+      
+      return html`
+      <li><h3>${location.name}</h3>
+        <p>hello</p>
+      </li>
+      ` 
+    })}
+    </ol>
+    </div>
+    </aside>`
 
   }
 
 
-
   render() {
 
-    // return html`as;d][as;]`
-    return html`<section id="map" style="
+    return html`
+
+    ${this.getAside()}
+
+    <button
+    @click=${this.openDrawer}
+    class="list-view">view</button>
+    <div style="
     width: 100vw;
     height: 100vh;
-    ">hello</section>`;
+    overflow: hidden;
+    ">
+
+    <section id="map"
+    width: 100vw;
+    height: 100vh;
+    ">
+    </section>
+    </div>
+    `;
   }
 }
 
--- src/components/Pages/Map.tsx
+++ src/components/Pages/Map.tsx
@@ -1,20 +1,28 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { createComponent } from "@lit/react";
-
 import {MapComponent as MapComponentWC} from "./Map-component"
-
-
-
-
+import styles from "./styles.module.css"
 
 export default function Map() {
-    
+
+
     const MapComponent = createComponent({
        tagName: 'map-component',
        elementClass: MapComponentWC,
        react: React,
      });
-    // return <div></div>
-    return <MapComponent></MapComponent>
+
+
+    return <div className={styles["relative"]}>
+ 
+
+
+
+
+{/* <button className={styles["list-view"]} */}
+ {/* onClick={() => {switchIsOpened((prev: boolean) => !prev)}}>view</button> */}
+    <MapComponent>
+    </MapComponent>
+    </div>
 }
 
