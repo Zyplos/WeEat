1d44d9cb38ada997fc68f7c6e2d073749f26f994
Angel <22824872+Zyplos@users.noreply.github.com>
2023-11-26 22:38:12 -0600

    intro image added back

--- src/pages/Setup/SetupIndex.tsx
+++ src/pages/Setup/SetupIndex.tsx
@@ -1,20 +1,32 @@
-import MainLayout from "../../components/MainLayout";
 import { RouterButton } from "../../components/Button";
-
-// import IntroImage from "../../assets/intro-picture.svg";
 
 export default function SetupIndex() {
   const isSetupDone = localStorage.getItem("setup-done") == "true";
 
+  // usually 100vh with no overflow is bad but in this case theres nothing
+  // important being cut off so its okay
+  // and this is the only page that uses these rules so other pages wont be affected
   return (
     <>
-      <MainLayout>
-        <h1>Welcome to WeEat!</h1>
-        <p>Looking for something to eat? On a budget? Short on time?</p>
-        <p>WeEat has you covered! Find places nearby based on what you are hungry for.</p>
-        <p>We will pick places for you or your group to go to within whatever time you have for breakfast, lunch, or dinner.</p>
-        {isSetupDone ? <RouterButton to="/preferences">Edit Preferences</RouterButton> : <RouterButton to="/preferences/categories">Get Started!</RouterButton>}
-      </MainLayout>
+      <div
+        style={{
+          maxHeight: "100vh",
+          overflow: "hidden",
+        }}
+      >
+        <main
+          style={{
+            padding: "var(--space-l)",
+          }}
+        >
+          <h1>Welcome to WeEat!</h1>
+          <p>Looking for something to eat? On a budget? Short on time?</p>
+          <p>WeEat has you covered! Find places nearby based on what you are hungry for.</p>
+          <p>We will pick places for you or your group to go to within whatever time you have for breakfast, lunch, or dinner.</p>
+          {isSetupDone ? <RouterButton to="/preferences">Edit Preferences</RouterButton> : <RouterButton to="/preferences/categories">Get Started!</RouterButton>}
+        </main>
+        <img src="/weeat-welcome.svg" alt="intro" />
+      </div>
     </>
   );
 }
